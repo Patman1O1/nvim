1c8e90efd6494590f440b0d88f3a7a07b82ecda0
Patman1O1 <patrickthomas101x@gmail.com>
2026-06-16 14:11:49 -0500

    Add test.sh

---
+++ tests/scripts/install-nvim/test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# nvim/tests/scripts/install-nvim/test.sh
+
+set -eu
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${0}")" >/dev/null 2>&1 && pwd)"
+NVIM_DIR="${SCRIPT_DIR}/../../.."
+
+echo "Running test suite for getdistro..."
+docker build \
+  --no-cache \
+  -t test-getdistro \
+  -f "${SCRIPT_DIR}/Dockerfile" \
+  "${NVIM_DIR}"
+
