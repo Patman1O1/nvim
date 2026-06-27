668edd2c59519f24cf61b9d741657acd91601683
Patrick Thomas <patrickthomas101x@gmail.com>
2026-06-27 13:07:27 -0500

    Add lazy

--- init.lua
+++ init.lua
@@ -1,5 +1,21 @@
-vim.g.mapleader = "/"
-vim.g.maplocalleader = "/"
+-- nvim/init.lua
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.fn.isdirectory(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
 require("config")
 require("plugins")
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
+})
