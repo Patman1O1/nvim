5d60b97b308ba5535def525a81f95e3a0a546fdb
Patrick Thomas <patrickthomas101x@gmail.com>
2026-06-27 13:06:57 -0500

    Add nvim-tree.lua

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,9 @@
+-- nvim/lua/plugins/nvim-tree.lua
+
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("nvim-tree").setup({})
+    end,
+}
