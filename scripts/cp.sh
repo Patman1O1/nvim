f0306020694d20890ad4a45b42446d6308da5aae
Patrick Thomas <patrickthomas101x@gmail.com>
2026-06-27 12:07:54 -0500

    Fix bugs

--- scripts/cp.sh
+++ scripts/cp.sh
@@ -8,34 +8,46 @@ FORCE_FLAG="${1:-}"
 TAB="    "
 REPO_DIR="$(cd -- "$(dirname -- "${0}")" >/dev/null 2>&1 && pwd)/.."
 
-# Ensure correct usage
-if [ "${#}" -gt 1 ] || [ "${#}" -eq 1 ] && { [ "${FORCE_FLAG}" != "-f" ] && [ "${FORCE_FLAG}" != "--force" ]; }; then
+# Functions
+show_usage() {
     printf "USAGE\n${TAB}${0} [options]\nOPTIONS\n${TAB}-f --force${TAB}Replace ~/.config/nvim\n"
+}
+
+copy() {
+    # Ensure ~/.config exists
+    mkdir -p "${HOME}/.config"
+
+    # Copy the GitHub repo into ~/.config/nvim
+    cp -r "${REPO_DIR}" "${HOME}/.config/nvim"
+
+    # Go to ~/.config/nvim
+    cd "${HOME}/.config/nvim"
+
+    # Remove unnecessary files and directories
+    rm -rf .git
+    rm -f .gitignore LICENSE README.md
+
+    # Let the user know the operation was successful
+    echo "Successfully copied the Neovim configuration to ${HOME}/.config"
+}
+
+if [ "${#}" -eq 1 ]; then
+    if [ "${1}" != "-f" ] && [ "${1}" != "--force" ]; then
+        show_usage
+	exit 1
+    fi
+
+    copy
+    exit 0
+elif [ "${#}" -eq 0 ]; then
+    if [ -e "${HOME}/.config/nvim" ]; then
+        printf "${0}: ${HOME}/.config/nvim already exists and is not empty\n"
+	exit 1
+    fi
+
+    copy
+    exit 0
+else
+    show_usage
     exit 1
 fi
-
-# Do nothing if ~/.config/nvim is not empty
-if [ -e "${HOME}/.config/nvim" ]; then
-    echo "${0}: ${HOME}/.config/nvim is not empty"
-    exit 1
-fi
-
-# Ensure ~/.config exists
-mkdir -p "${HOME}/.config"
-
-# Ensure ~/.config/nvim is removed
-rm -rf "${HOME}/.config/nvim"
-
-# Copy every from the GitHub repo into ~/.config/nvim
-cp -r "${REPO_DIR}" "${HOME}/.config/nvim"
-
-# Go to the copied Neovim directory
-cd "${HOME}/.config/nvim"
-
-# Remove unnecessary files and directories
-rm -rf .git
-rm -f .gitignore LICENSE README.md
-
-# Let the user know the operation was successful
-echo "Successfully copied the Neovim configuration to ${HOME}/.config"
-exit 0
