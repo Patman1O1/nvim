9e3ed6387631a386c435e5e420bcc471013c8faa
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-17 23:45:33 -0500

    Add lsp.lua

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,82 @@
+return {
+  -- Package Manager for LSP servers, linters, and formatters
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  
+  -- Bridge between Mason and nvim-lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        -- Automatically install these servers if not present
+        ensure_installed = { "lua_ls", "ts_ls", "pyright" }, 
+      })
+    end,
+  },
+
+  -- Core LSP Configurations and Server Attachment
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { 
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp" -- Connects LSP to autocomplete
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Keymaps to trigger when an LSP connects to a file
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)      -- Go to definition
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)           -- Show documentation
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- Rename symbol
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)     -- Find references
+        end,
+      })
+
+      -- Set up servers using mason-lspconfig's handler
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+      })
+    end,
+  },
+
+  -- Autocompletion Engine
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip", -- Snippet engine required for nvim-cmp
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args) require("luasnip").lsp_expand(args.body) end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+        }),
+      })
+    end,
+  }
+}
+
