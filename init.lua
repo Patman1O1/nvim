887528ed94d160e9d399d003cd65b87af4790126
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-11 15:21:00 -0500

    Fix lazy_config path

--- init.lua
+++ init.lua
@@ -1,20 +1,37 @@
--- Bootstrap lazy  
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- Bootstrap plugins
-require("config")
-require("plugins")
+local lazy_config = require "config.lazy"
+
+-- load plugins
 require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-    },
-})
+  {
+    "Patman1O1/neovim",
+    lazy = false,
+    branch = "main",
+    import = "plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
