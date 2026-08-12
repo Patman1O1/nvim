0a86115875e88486c21e3b39463d8b7c800eca7f
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-12 08:07:11 -0500

    Remove nvim-tree.lua

--- lua/plugins/nvim-tree.lua
+++
@@ -1,9 +0,0 @@
--- nvim/lua/plugins/nvim-tree.lua
-
-return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("nvim-tree").setup({})
-    end,
-}
