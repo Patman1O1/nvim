f8d413316ef77eb351fee91f0971b8017985960e
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 10:02:09 -0500

    Add c.lua

---
+++ lua/configs/langs/c.lua
@@ -0,0 +1,10 @@
+return {
+  ts_parser = "c",
+  lsp_name = "clangd",
+  setup_lsp = function()
+    vim.lsp.config("clangd", {
+      cmd = { "clangd", "--background-index", "--clang-tidy" },
+    })
+    vim.lsp.enable("clangd")
+  end,
+}
