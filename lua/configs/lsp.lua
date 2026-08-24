647705aee3de1512ff81535f577e789024e11c9f
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-24 10:40:22 -0500

    Update langs references

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,30 +1,30 @@
--- lua/configs/languages.lua
+-- lua/configs/lsp.lua
 local M = {
   ts_parsers = { "vim", "vimdoc" }, -- base defaults
   lsps = {},
 }
 
 -- Target directory path
-local lang_dir = vim.fn.stdpath("config") .. "/lua/configs/langs"
+local lsp_dir = vim.fn.stdpath("config") .. "/lua/configs/lsp"
 
--- Scan all .lua files inside lua/configs/languages/
-local handle = vim.uv.fs_scandir(lang_dir)
+-- Scan all .lua files inside lua/configs/lsp/
+local handle = vim.uv.fs_scandir(lsp_dir)
 if handle then
   while true do
     local name, type = vim.uv.fs_scandir_next(handle)
     if not name then break end
     if type == "file" and name:sub(-4) == ".lua" then
       local mod_name = name:sub(1, -5)
-      local lang = require("configs.langs." .. mod_name)
+      local lsp = require("configs.lsp." .. mod_name)
 
       -- Collect Treesitter parsers
-      if lang.ts_parser then
-        table.insert(M.ts_parsers, lang.ts_parser)
+      if lsp.ts_parser then
+        table.insert(M.ts_parsers, lsp.ts_parser)
       end
 
       -- Collect LSP initializers
-      if lang.setup_lsp then
-        table.insert(M.lsps, lang.setup_lsp)
+      if lsp.setup_lsp then
+        table.insert(M.lsps, lsp.setup_lsp)
       end
     end
   end
