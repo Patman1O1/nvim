b4e980bd812b1f0b7c960821c08a9ff5799399d9
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-17 09:28:14 -0500

    Add purge.sh

---
+++ scripts/purge.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -eu
+
+rm -rf "$HOME/.config/nvim"
+rm -rf "$HOME/.local/share/nvim"
+rm -rf "$HOME/.local/lib/nvim"
+rm -rf "$HOME/.local/state/nvim"
+rm -f "$HOME/.local/bin/nvim"
