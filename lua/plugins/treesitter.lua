b9e875b0b52aedf207bbd5628ae1e7f54140b0ad
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-19 13:29:25 -0500

    Edit treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
-local lsps = require("configs.lsp")
-
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = lsps.ts_parsers,
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  opts = function(_, opts)
+    opts.ensure_installed =
+      vim.list_extend(opts.ensure_installed or {}, require("configs.lspconfig").ts_parsers())
+    opts.highlight = { enable = true }
+    opts.indent = { enable = true }
+    return opts
+  end,
 }
